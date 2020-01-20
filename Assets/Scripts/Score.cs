1b0a4d215595729030b29fd222d327de09b9ac14
Marvin <marvin.k.scharding@gmail.com>
2020-01-20 20:14:26 +0100

    added first score UI

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+
+    // [SerializeField] private Transform player;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        scoreText.text = score.ToString("F1");
+    }
+  
+}
--- Assets/Scripts/collectibles.cs
+++ Assets/Scripts/collectibles.cs
@@ -16,6 +16,7 @@ public class collectibles : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Score.score += 10;
         Destroy(gameObject);
     }
 }
